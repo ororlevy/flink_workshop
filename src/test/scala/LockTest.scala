5be8e7f7a9524057aa1cf4e74d64481466ee0ca1
ororlevy <or.levy@contentsquare.com>
2021-06-15 11:01:09 +0300

    Stage four - fixes

--- src/test/scala/LockTest.scala
+++ src/test/scala/LockTest.scala
@@ -42,11 +42,4 @@ class LockTest extends AnyFlatSpec {
     val sum = MagicLock.solve(Seq(move, move, move))
     assert(sum.toInt == 559)
   }
-
-  "dd" should "sdf" in {
-    val d = "  \\ \\_\\ .----------.     /\n   \\   (  ||MARS||  )   /..\n|\\  \\   ~-||====||-~_  ///\\\\\n| \\  \\    ||    || // /((()))\n \\|  |    ||====|| ~ / \\\\\\///\n     |__  ||    ||   |  `|''\n     | |\\ ||====||___|  _|_\n     | | \\`'    `/ | |  =O=\n   V | |C|_ `   /  | |   ~\n   E | |E||\\___/   | |_ J\n _ N | |R|| ___    | |_]U\n| |U | |E|l/:::\\   | |  P\n|/ S | |S|::::::\\  | |  I\n     | | /:::::::\\ | |  T\n     |_|/:::::::::\\|_|  E\n  /|/|:.:.:.:.:.:.:.:|  R\n | / |.:.:.:.:.:.:.:.|\n |/| /....READ.......\\ |\\\n   |/.......THE.......\\ \\|\n   /. . . . . LOGS . . .\\\n  /. . . . . . . . . . .\\\nYou have unchained the torch and keep walking inside the maze. \nYou see in front of you many paths, and each hallway has a sign: \n 1. Jupiter\n 2. Ceres\n 3. Mars\n 4. Venus\nYou can go through a hallway you choose, and each one holds a mystery.\nSome will have traps, and one of them holds a secret treasure!\nKeep walking in each way by sending the sign's initial letter with the direction. \n\n          [_W_]   \n     [_A_][_S_][_D_]\n\nW - forward , S - backword\nD - Turn right, A - Turn Left\n\nFor example: To go into Mars attic -> M-W.  \nModify your stream to use Keyed Stream"
-    val c = Util.Formatter.encode(d)
-    print(c)
-  }
-
 }
